41e5a7ce53a18b98da6f40c3989b340d50e0ca54
Glen Goulter <glenrg@gmail.com>
2018-02-02 17:43:03 -0800

    a wall, like facebook, I can register, login, post and comment.

--- server.py
+++ server.py
@@ -5,43 +5,53 @@ import re
 import md5
 app = Flask(__name__)
 app.secret_key = "ThisIsSecret!"
-mysql = MySQLConnector(app,'dbloginregistration')
+mysql = MySQLConnector(app,'dbwall')
 
 # our index route will handle rendering our form
 
 #1
 @app.route('/')
 def index():
-  #session['date'] =  datetime.now().strftime("%m/%d/%Y")
   return render_template("registrationForm.html")
 
-# this route will handle our form submission
-# notice how we defined which HTTP methods are allowed by this route
-# WE ARE WAITING FOR A POST REQUEST
+@app.route('/insertComment', methods=['POST'])
+def insertComment():
+  query = "INSERT INTO tblComments (message_id, user_id, comment, created_at, updated_at) VALUES (:message_id, :user_id, :comment, NOW(), NOW())"
+  data = {
+            'comment': request.form['commentcontent'],
+            'message_id': request.form['messageid'],
+            "user_id":session["uid"]
+        }
+
+  row_id = mysql.query_db(query, data) #when there is an insert, we get back the id of the row inserted to.
+  #session['uid']=user #this means we have a logged in user.
+  #myUsers = mysql.query_db("SELECT * FROM tblusers")
+  return redirect('/wall')
+
+@app.route('/insertMessage', methods=['POST'])
+def insertMessage():
+  query = "INSERT INTO tblMessages (message, user_id, created_at, updated_at) VALUES (:message, :user_id, NOW(), NOW())"
+  data = {
+            'message': request.form['messagecontent'],
+            "user_id":session["uid"]
+        }
+
+  row_id = mysql.query_db(query, data) #when there is an insert, we get back the id of the row inserted to.
+  #session['uid']=user #this means we have a logged in user.
+  #myUsers = mysql.query_db("SELECT * FROM tblusers")
+  return redirect('/wall')
+
+
 @app.route('/process', methods=['POST'])
 def process():
   #do validation
   #setup the regex to check for just letters in the first and last names.
   ALPHA_REGEX = re.compile(r'^[a-zA-Z]+$')
   EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
-  #some test values
-  #A23456789
-  #23456789A
-  #ABCDEFGh9
-  #9ABCDEFGh
-  PW_U_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]*[A-Z]+[a-zA-Z0-9.+_-]*$')
-  PW_NUM_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]*[0-9]+[a-zA-Z0-9.+_-]*$')
-  BDAY_VALID_REGEX = re.compile(r'^[0-9]{2}\/{1}[0-9]{2}\/[0-9]{4}$')
   
   confPw = request.form['confpassword']
   query = "INSERT INTO tblusers (first_name, last_name, email, password, created_at, updated_at) VALUES (:fname, :lname, :email, :pw, NOW(), NOW())"
   
-  # 1. First Name - letters only, at least 2 characters and that it was submitted
-  # 2. Last Name - letters only, at least 2 characters and that it was submitted
-  # 3. Email - Valid Email format, and that it was submitted
-  # 4. Password - at least 8 characters, and that it was submitted
-  # 5. Password Confirmation - matches password
-
   if not request.form['firstname'] or not request.form['lastname'] or not request.form['email'] or not request.form['password'] :
     flash(u"You have not entered all the required data",'flashErrorMessages')
     return redirect('/') 
@@ -89,17 +99,22 @@ def process():
       new_user = mysql.query_db(query, data) #when there is an insert, we get back the id of the row inserted to.
       session['uid']=new_user #this means we have a logged in user.
       myUsers = mysql.query_db("SELECT * FROM tblusers")
-      return redirect('/dashboard')
+      return redirect('/wall')
 
-@app.route('/dashboard') # this is the success of a register!
+@app.route('/wall') # this is the success of a register!
 def dashboard():
-  query = "SELECT * FROM tblusers WHERE id = :user_id" #being able to show this dashboard means there is an active session, someone is logged in.
-  data = {
-          "user_id":session["uid"]
-      }   
-  user = mysql.query_db(query, data) #when there is a select, we get back a list of dictionaries converted from the rows of data selected.
-  print user
-  return render_template("dashboard.html", user = user[0]) 
+    #THIS IS A GIANT BLOB OF MESSAGE DATA AND COMMENT DATA, BUT NO COMMENT USER NAMES.    
+    #query= mysql.query_db("select tblusers.first_name, tblmessages.message, tblmessages.user_id, tblmessages.id, tblmessages.created_at, tblmessages.updated_at, tblcomments.message_id, tblcomments.user_id, tblcomments.comment, tblcomments.created_at, tblcomments.updated_at from tblmessages join tblusers on tblmessages.user_id = tblusers.id left join tblcomments on tblmessages.id = tblcomments.message_id;")
+
+    messageQuery= mysql.query_db("select tblusers.first_name, tblmessages.message, tblmessages.created_at, tblmessages.id from tblmessages join tblusers on tblmessages.user_id = tblusers.id")
+    commentQuery=mysql.query_db("select tblcomments.comment, tblcomments.created_at, tblusers.first_name, tblcomments.message_id from tblcomments left join tblusers on tblcomments.user_id = tblusers.id")
+    query2 = "SELECT * FROM tblusers WHERE id = :user_id" #being able to show this dashboard means there is an active session, someone is logged in.
+    data = {
+            "user_id":session["uid"]
+        }   
+    user = mysql.query_db(query2, data) #when there is a select, we get back a list of dictionaries converted from the rows of data selected.
+    return render_template("wall.html", user = user[0], messages=messageQuery, comments=commentQuery) 
+
   #when dashboard.html access user.first_name, it's from user.  
   #USER from mysql.query_db(query, data) IS an list of one dictionary. user[0] is a dictioary.
   #[{u'first_name': u'Tom', u'last_name': u'Jones', u'created_at': datetime.datetime(2018, 2, 1, 17, 48, 40), 
@@ -113,23 +128,16 @@ def login():
 	}
 	user = mysql.query_db(query, data) # []
 	print user
-
 	
 
 	if len(user) > 0:
 		user = user[0]
 		if user["password"] ==  md5.new(request.form['password']).hexdigest():
 			session["uid"] = user["id"]
-			return redirect("/dashboard")
+			return redirect("/wall")
 	flash("Email and password not found")
 	return redirect("/")
 
-	# 	else:
-	# 		flash("Incorrect Password")
-	# 		return redirect("/")
-	# else:
-	# 	flash("No email found")
-# 	return redirect("/")
 
 #fresh login method one
 # @app.route('/login', methods=['GET'])
